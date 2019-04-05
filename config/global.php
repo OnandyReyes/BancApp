15d102027e32ffb3c9e5c9d5778e8ff139f690e0
onandy <odalis_javier@hotmail.com>
2019-04-05 12:54:16 -0400

    global

--- config/global.php
+++ config/global.php
@@ -4,7 +4,7 @@ define("DB_HOST","localhost");
 
 //Nombre de la base de datos
 //define("DB_NAME", "ayrbussi_prestared");
-define("DB_NAME", "prestared");
+define("DB_NAME", "bancapp");
 
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
@@ -18,5 +18,5 @@ define("DB_PASSWORD", "");
 define("DB_ENCODE", "utf8");
 
 //Definimos una constante como nombre del proyecto
-define("PRO_NOMBRE", "PrestaRed");
+define("PRO_NOMBRE", "BancApp");
 ?>
